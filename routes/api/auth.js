4a138b585ab7a7fe6f098ee5e62d39a74b900aff
Manuel Novoa <linguini@Manuels-iMac.local>
2019-06-20 17:35:15 -0500

    MNovoa2 initial commit...

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // @route GET api/auth
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route...'))
+router.get('/', (req, res) => res.send('Auth route...'));
 
 
 module.exports = router;
