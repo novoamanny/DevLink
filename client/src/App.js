9fa84d668bf479053f61298e43192aeb35724f87
Manuel Novoa <novoamanuel@ymail.com>
2019-07-03 12:22:22 -0500

    DevLink API is working properly and has been tested. Users are able to post and comment/like... Front-end development has started...

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, {Fragment, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+// import store from './store';
+// import { loadUser } from './actions/auth';
+// import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Manuel!!!</h1>
+    </div>
+  );
+}
+
+export default App;
