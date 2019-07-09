7abf64e7017dfb907387d25acdb5c121253e1278
Manuel Novoa <novoamanuel@ymail.com>
2019-07-09 14:28:29 -0500

    Nav button and logo has been added...

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,40 +1,42 @@
 import React, {Component} from 'react';
 
-// Colors
-const colors = {
-    primary: '#49a34c',
-    secondary: '#17a2b8',
-    lightBlue: '#61dafb',
-    dark: '#343a40',
-    light: '#f4f4f4',
-    danger: '#dc3545',
-    success: '#28a745',
-}
+import classes from './Navbar.css';
 
-// Style Sheet
-let styles = {
-    nav:{
-        backgroundColor: colors.primary,
-        width: '100%',
-        position: 'fixed',
-        top: 0,
-        zIndex: 1
-    },
-    links:{
 
-    },
-    logo:{
-        
-    }
-    
-}
+
+
 
 
 const Navbar = () =>{
     return(
-        <nav style={styles.nav}>
-            <i className="fas fa-laptop-code"></i>
+        <nav>
+            <div className='contents'>
+                <div className="logoContainer">
+                    <i className="fas fa-laptop-code logo"></i>
+                    
+                    
+                    <h1>devLink</h1>
+                </div>
             
+                    
+                        
+                
+                
+                
+                
+
+                <div className='btnContainer'>
+                    {/* <div className='top'></div>
+                    <div className='middle'></div>
+                    <div className='bottom'></div> */}
+
+                    <span>&#9776;</span>
+                </div>
+            </div>
+
+
+
+            <div className='underline'></div>
         </nav>
     );
 }
